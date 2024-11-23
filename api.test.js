37f6327dbbc657c7fff0040345d73b4ab34b19ff
wangshu24 <trandinhphuc1997@gmail.com>
2024-11-23 21:57:07 +1300

    wrote logic for fetch n number of pages, based on batch fetch API of size 10, added some test cases and setup basic test file, test to be added later

--- api.test.js
+++ api.test.js
@@ -1,2 +1,7 @@
+import test from 'node:test'
+import { FetchAPI } from './pagination'
+
 const jest = require('jest')
+
+const fetching = new FetchAPI();
 
--- pagination.js
+++ pagination.js
@@ -1,5 +1,5 @@
-PAGE_SIZE = 10
-MAX_PAGE = 113
+const PAGE_SIZE = 10
+const MAX_PAGE = 113
 
 function fetchPage(n) {
     let results = []
@@ -14,14 +14,51 @@ function fetchPage(n) {
     }
 }
 
-class FetchAPI {
-    constructor() {
-        this.remainder = 0
-        this.startFrom = 0 
-    }
-
+export class FetchAPI {    
+    remainder = []
+    nextPage = 0     
+    
     fetch(n) {
-        let res = fetchPage(n).results
+        let res = []
+        if (this.remainder.length === 0) {
+            let newFetch = fetchPage(this.nextPage)
+            let i=0
+            while (i < n){
+                res.push(newFetch.results.shift())
+                i++
+            }
+            this.remainder = newFetch.results
+            this.nextPage = newFetch.nextPage
+            return res
+        }
+
+        if (this.remainder.length > 0 && n <= this.remainder.length) {
+            for (let i=0; i < n; i++) {
+                let page = this.remainder.shift()
+                res.push(page)
+
+            }
+            
+            return res
+        } 
+        
+        res = res.concat(this.remainder)
+        this.remainder = []
+        let nextPage = res[res.length-1]+1
+        const destPage = res[0]+n
+
+        for (let i = Math.floor(nextPage/10); i < Math.floor(destPage/10)+1;i++) {
+            let newFetch = fetchPage(i)
+            this.remainder = this.remainder.concat(newFetch.results)
+            this.nextPage = newFetch.nextPage 
+        }
+
+        let i= res.length
+        while (i < n){
+            res.push(this.remainder.shift())
+            i++
+        }
+        
         return res
     };
 }
@@ -29,6 +66,8 @@ class FetchAPI {
 
 //Write test cases
 const fetching = new FetchAPI();
-console.log(fetching.fetch(0))
 console.log(fetching.fetch(1))
-console.log(fetching.fetch(2))
+console.log(fetching.fetch(1))
+console.log(fetching.fetch(5))
+console.log(fetching.fetch(7))
+console.log(fetching.fetch(9))

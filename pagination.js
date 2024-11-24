838c445f08b1fe7d5b213a3d70af72f259a527be
wangshu24 <trandinhphuc1997@gmail.com>
2024-11-24 19:48:49 +1300

    added offset pagination API, rename fetch API to pageBasedFetching

--- pagination.js
+++ pagination.js
@@ -1,9 +1,9 @@
 const PAGE_SIZE = 10
 const MAX_PAGE = 113
 
-function fetchPage(n) {
+function fetchPages(n) {
     let results = []
-    for (let i = n * PAGE_SIZE; i < MAX_PAGE - (n * PAGE_SIZE); i++) {
+    for (let i = n * PAGE_SIZE; i < MAX_PAGE ; i++) {
         if (results.length === 10) { break }
         results.push(i)
     }
@@ -18,56 +18,71 @@ export class FetchAPI {
     remainder = []
     nextPage = 0     
     
-    fetch(n) {
-        let res = []
+    pageBasedFetch(n) {
+        let result = []
         if (this.remainder.length === 0) {
-            let newFetch = fetchPage(this.nextPage)
+            let newFetch = fetchPages(this.nextPage)
             let i=0
             while (i < n){
-                res.push(newFetch.results.shift())
+                result.push(newFetch.results.shift())
                 i++
             }
             this.remainder = newFetch.results
             this.nextPage = newFetch.nextPage
-            return res
+            return result
         }
 
         if (this.remainder.length > 0 && n <= this.remainder.length) {
             for (let i=0; i < n; i++) {
                 let page = this.remainder.shift()
-                res.push(page)
+                result.push(page)
 
             }
             
-            return res
+            return result
         } 
         
-        res = res.concat(this.remainder)
+        result = result.concat(this.remainder)
         this.remainder = []
-        let nextPage = res[res.length-1]+1
-        const destPage = res[0]+n
+        let nextPage = result[result.length-1]+1
+        const destPage = result[0]+n
 
         for (let i = Math.floor(nextPage/10); i < Math.floor(destPage/10)+1;i++) {
-            let newFetch = fetchPage(i)
+            let newFetch = fetchPages(i)
             this.remainder = this.remainder.concat(newFetch.results)
             this.nextPage = newFetch.nextPage 
         }
 
-        let i= res.length
+        let i= result.length
         while (i < n){
-            res.push(this.remainder.shift())
+            result.push(this.remainder.shift())
             i++
         }
-        
-        return res
+        return result
     };
+
+    offsetFecthing(offset){
+        //Assuming API returns the remainder pages 
+        //from the point of the offset within the pageset
+
+        let pageSetQuery = fetchPages(Math.floor(offset/PAGE_SIZE))
+        let res = []
+        pageSetQuery.results.map((p) => {
+            if (p > offset){res.push(p)}
+        })
+
+        return res
+    }
 }
 
+//Write test cases: Page-Based Fetching
+const pageBasedFetch = new FetchAPI();
+console.log(pageBasedFetch.pageBasedFetch(1))
+console.log(pageBasedFetch.pageBasedFetch(1))
+console.log(pageBasedFetch.pageBasedFetch(5))
+console.log(pageBasedFetch.pageBasedFetch(7))
+console.log(pageBasedFetch.pageBasedFetch(9))
 
-//Write test cases
-const fetching = new FetchAPI();
-console.log(fetching.fetch(1))
-console.log(fetching.fetch(1))
-console.log(fetching.fetch(5))
-console.log(fetching.fetch(7))
-console.log(fetching.fetch(9))
+//Write test cases: Offset Fetching 
+const offsetFetching = new FetchAPI()
+console.log(offsetFetching.offsetFecthing(87))
